2af97226abe1ee268fb774ada18e2f1568ab050b
Renzo Carbonara <renzo.carbonara@iohk.io>
2018-03-29 20:12:18 +0200

    docs

--- wallet-new/default.nix
+++ wallet-new/default.nix
@@ -2,16 +2,8 @@
 # dependencies needed to work on `wallet-new` using `Cabal` are available.
 #
 # Running `nix-build` builds the `wallet-new` code.
-#
-# Since this file is intended for development purposes, by default we enable
-# profiling for dependencies.
-
-{ enableProfiling ? true
-, enableDebugging ? true
-}@args:
-
 let
-  drv = (import ../. args).cardano-sl-wallet-new;
+  drv = (import ../. {}).cardano-sl-wallet-new;
 in
   if ((import <nixpkgs> {}).stdenv.lib.inNixShell)
     then drv.env
